65a541fc129fe98a02c8827e939f9c41ec546907
Viktoriia Melanchenko <victoriamelanchenko@gmail.com>
2020-12-13 01:43:08 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Component } from 'react';
 
 import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 
+import Statistics from './components/Statistics/Statistics';
+
+import Notification from './components/Notification/Notification';
+
 class App extends Component {
   state = {
     good: 0,
@@ -14,7 +18,21 @@ class App extends Component {
     this.setState((prevState) => ({ [feedback]: prevState[feedback] + 1 }));
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total ? Math.round((good * 100) / total) : 0;
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
     console.log(this.state);
     return (
       <div>
@@ -23,6 +41,15 @@ class App extends Component {
           options={['good', 'neutral', 'bad']}
           onLeaveFeedback={this.onHandleIncrement}
         />
+        <h1>Statistics</h1>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+        <Notification message="No feedback given" />
       </div>
     );
   }
